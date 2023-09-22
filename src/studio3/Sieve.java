61202323befbe02cb628f9a19d5ffef641079e0f
soray <soray@DESKTOP-IPRP9PJ.dhcp.wustl.edu>
2023-09-22 01:27:15 -0500

    Studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,27 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the highest number? "); 
+		int n = in.nextInt();
+		int [] sieve = new int [n - 1]; 
+		boolean [] isPrime = 
+				
+		for (int i = 0; i < sieve.length; i++) {
+			if (isPrime [p]) {
+				for (int i = p * p <= limit; i += p) {
+					
+				}
+			}
+			
+			 
+		}
+
+	}
+
+}
